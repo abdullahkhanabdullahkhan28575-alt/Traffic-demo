1faf7212c01c456a8608018fc522a5b394650a68
Abdullah <abdullahkhanabdullahkhan28575@gmail.com>
2026-08-13 10:22:55 +0530

    Delete JS.js

--- JS.js
+++
@@ -1,9 +0,0 @@
-
-function turnOn(){
-
-    document .getElementById("bulb").src="https://t4.ftcdn.net/jpg/18/81/82/23/240_F_1881822371_hSbjXOXZukzk9zWdwOGEnEsPfgTjdCmN.jpg"
-}
-function turnOff(){
-
-    document .getElementById("bulb").src="https://t4.ftcdn.net/jpg/18/81/82/23/240_F_1881822371_hSbjXOXZukzk9zWdwOGEnEsPfgTjdCmN.jpg"
-}
